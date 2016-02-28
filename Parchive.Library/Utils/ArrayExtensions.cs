f9ad20a194b3903b5a994da73bee7b32b49a4cd8
Gunnar Lilleaasen <ginnus@outlook.com>
2016-02-28 01:46:45 +0100

    Added comments to ArrayExtensions.

--- Parchive.Library/Utils/ArrayExtensions.cs
+++ Parchive.Library/Utils/ArrayExtensions.cs
@@ -8,6 +8,13 @@ namespace Parchive.Library
 {
     public static class ArrayExtensions
     {
+        /// <summary>
+        /// Sets all values in the array.
+        /// </summary>
+        /// <typeparam name="T">The array type.</typeparam>
+        /// <param name="array">The array.</param>
+        /// <param name="value">The value to set.</param>
+        /// <returns>The array.</returns>
         public static T[] SetAllValues<T>(this T[] array, T value) where T : struct
         {
             for (int i = 0; i < array.Length; i++)
